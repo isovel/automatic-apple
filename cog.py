9682564ed8b5f486dc3fe03d7b936e600ba8b6dd
aejb <aejb>
2020-04-08 19:27:15 +0100

    adds basic modlog functionality

--- cog.py
+++ cog.py
@@ -16,6 +16,9 @@ class moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def has_mod(ctx):
+        return ctx.guild.get_role(569846811796176916) in ctx.author.roles
+
     @commands.command()
     async def ping(self, ctx):
         """------ a simple ping-pong command"""
@@ -24,13 +27,23 @@ class moderation(commands.Cog):
         embed.add_field(name=latency_rounded,
                         value="pong!",
                         inline=True)
-
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def find(self, ctx, user_mention: discord.Member):
-        await user_mention.add_roles(604623818358652951)
-        await ctx.send(str(user_mention))
+    @commands.has_role(569846811796176916)
+    async def verify(self, ctx, member: discord.Member):
+        """------ adds the verified role to a user"""
+        await member.add_roles(member.guild.get_role(604623818358652951))
+        await ctx.message.add_reaction('\N{WHITE HEAVY CHECK MARK}')
+
+    @verify.error
+    async def verify_error(self, ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            await ctx.send('You do not have the permissions to verify a user.')
+            await ctx.message.add_reaction('\N{CROSS MARK}')
+        if isinstance(error, commands.BadArgument):
+            await ctx.send('That user was not found in this guild. Have an apple.')
+            await ctx.message.add_reaction('\N{CROSS MARK}')
 
 def setup(bot):
     bot.add_cog(moderation(bot))
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import discord
 from discord.ext import commands
 import traceback
 import sys
+from datetime import datetime
+
+now = datetime.now()
 
 initial_extensions = ['cog']
 
@@ -39,5 +42,50 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('That command does not exist, or the cog did not properly load.')
+        await ctx.message.add_reaction('\N{CROSS MARK}')
+    else:
+        raise error
+
+
+@bot.event
+async def on_message_delete(ctx):
+    await ctx.guild.get_channel(695630766477934632).send(
+        "Message by `{author}` deleted from `{channel}` at `{time}`\n > {content}".format(author=ctx.author,
+                                                                                          channel=ctx.channel.name,
+                                                                                          time=now.strftime(
+                                                                                              "%I:%M:%S%p"),
+                                                                                          content=discord.utils.escape_markdown(
+                                                                                              ctx.content)))
+
+
+@bot.event
+async def on_message_edit(before: discord.Message, after: discord.Message):
+    if len(str(discord.utils.escape_markdown(before.content))) < 2000 and len(
+            str(discord.utils.escape_markdown(after.content))) < 2000:
+        await before.guild.get_channel(695630766477934632).send("Message by `{author}` edited in `{channel}` at `"
+                                                                "{time}`\n**Before:**\n> {before}\n**After:**\n> "
+                                                                "{after}".format(author=before.author,
+                                                                                 channel=before.channel.name,
+                                                                                 time=now.strftime("%I:%M:%S%p"),
+                                                                                 before=discord.utils.escape_markdown(
+                                                                                     before.content),
+                                                                                 after=discord.utils.escape_markdown(
+                                                                                     after.content)))
+    else:
+        await before.guild.get_channel(695630766477934632).send("Message by `{author}` edited in `{channel}` at `"
+                                                                "{time}`".format(author=before.author,
+                                                                                 channel=before.channel.name,
+                                                                                 time=now.strftime("%I:%M:%S%p")))
+        await before.guild.get_channel(695630766477934632).send(
+            "**Before**\n> {before}".format(before=discord.utils.escape_markdown(before.content)))
+        await before.guild.get_channel(695630766477934632).send(
+            "**After**\n> {after}".format(after=discord.utils.escape_markdown(after.content)))
+
 
 bot.run(bot_token)
