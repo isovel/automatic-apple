7467fc1364bdc23ded033226904b457e4e54fffa
aejb <aejb>
2020-04-09 21:04:14 +0100

    adds an apple command

--- cog.py
+++ cog.py
@@ -9,6 +9,7 @@ import aiohttp
 import asyncio
 from discord.ext import commands, tasks
 from datetime import datetime, timedelta
+from random import randint
 
 
 class moderation(commands.Cog):
@@ -44,6 +45,25 @@ class moderation(commands.Cog):
         if isinstance(error, commands.BadArgument):
             await ctx.send('That user was not found in this guild. Have an apple.')
             await ctx.message.add_reaction('\N{CROSS MARK}')
+
+    @commands.command()
+    async def pls(self, ctx):
+        """------ gives you an apple"""
+        roll=randint(1, 101)
+        if roll==1:
+            await ctx.send("woah! you found an extra shiny apple! ✨🍎✨")
+        elif roll==2 or roll==3:
+            await ctx.send("oh my god you found a GOLD APPLE! 🌟🌟🍎🌟🌟")
+        elif 4<=roll<=14:
+            await ctx.send("hey nice apple dude! 💫🍎💫")
+        elif roll==15 or roll==16:
+            await ctx.send("oh dear. you found a rotten apple :( 🤢🍎🤢")
+        elif 17<=roll<=25:
+            await ctx.send("oh neat! two apples! 🍎🍎")
+        elif 26<=roll<=30:
+            await ctx.send("you found a green apple. it is the enemy. remove it at once. 🚫🍏🚫")
+        else:
+            await ctx.send("here u go: 🍎")
 
 def setup(bot):
     bot.add_cog(moderation(bot))
