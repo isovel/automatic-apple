b43abbe27ed193c3b21f8ab2e6b85f3f113f0c32
toastythetoaster <grwdragon@gmail.com>
2021-04-12 21:10:24 -0400

    things, and stuff
    i was held at gunpoint and told to write this:
    emotional support provided by: HeroGamers#0900

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 __authors__ = 'aejb'
 
-## requires d.py 1.0
-## requires python3.6
+# requires d.py 1.0
+# requires python3.6
 import discord
 from discord.ext import commands
 import traceback
 import sys
 from datetime import datetime
-import pickledb
-
 
 initial_extensions = ['cog', 'apple']
 
 
-def gettoken():
-    token_file = open("token.txt", "r")
-    token_string = token_file.read()
-    token_token = token_string.split("\n")
-    bot_token = str(token_token[0])
-    return bot_token
+def get_token():
+    with open("token.txt", "r") as token_file:
+        token_string = token_file.read()
+        token_token = token_string.split("\n")
+        token = str(token_token[0])
+        return token
 
-
-bot_token = gettoken()
 
 description = "here to help with your apples."
 bot = commands.Bot(command_prefix=["apl "], description=description)
@@ -34,6 +30,7 @@ if __name__ == '__main__':
         except Exception as e:
             print("Failed to load" + extension, file=sys.stderr)
             traceback.print_exc()
+
 
 @bot.event
 async def on_ready():
@@ -56,51 +53,33 @@ async def on_command_error(ctx, error):
 @bot.event
 async def on_message_delete(ctx):
     now = datetime.now()
-    await ctx.guild.get_channel(695630766477934632).send(
-        "Message by `{author}` deleted from `{channel}` at `{time}`\n > {content}".format(author=ctx.author,
-                                                                                          channel=ctx.channel.name,
-                                                                                          time=now.strftime(
-                                                                                              "%I:%M:%S %p"),
-                                                                                          content=discord.utils.escape_markdown(
-                                                                                              ctx.content)))
+    await ctx.guild.get_channel(695630766477934632).send(f"Message by `{ctx.author}` deleted from `{ctx.channel.name}` at `{now.strftime('%I:%M:%S %p')}`\n > {discord.utils.escape_markdown(ctx.content)}")
 
 
 @bot.event
 async def on_message_edit(before: discord.Message, after: discord.Message):
-    if before.content==after.content: return
+    if before.content == after.content: return
     if len(str(discord.utils.escape_markdown(before.content))) < 2000 and len(
             str(discord.utils.escape_markdown(after.content))) < 2000:
         now = datetime.now()
-        await before.guild.get_channel(695630766477934632).send("Message by `{author}` edited in `{channel}` at `"
-                                                                "{time}`\n**Before:**\n> {before}\n**After:**\n> "
-                                                                "{after}".format(author=before.author,
-                                                                                 channel=before.channel.name,
-                                                                                 time=now.strftime("%I:%M:%S %p"),
-                                                                                 before=discord.utils.escape_markdown(
-                                                                                     before.content),
-                                                                                 after=discord.utils.escape_markdown(
-                                                                                         after.content)))
+        await before.guild.get_channel(695630766477934632).send(f"Message by `{before.author}` edited in `{before.channel.name}` at `{now.strftime('%I:%M:%S %p')}`\n**Before:**\n> {discord.utils.escape_markdown(before.content)}\n**After:**\n> {discord.utils.escape_markdown(after.content)}")
     else:
         now = datetime.now()
-        await before.guild.get_channel(695630766477934632).send("Message by `{author}` edited in `{channel}` at `"
-                                                                "{time}`".format(author=before.author,
-                                                                                 channel=before.channel.name,
-                                                                                 time=now.strftime("%I:%M:%S %p")))
-        await before.guild.get_channel(695630766477934632).send(
-            "**Before**\n> {before}".format(before=discord.utils.escape_markdown(before.content)))
-        await before.guild.get_channel(695630766477934632).send(
-            "**After**\n> {after}".format(after=discord.utils.escape_markdown(after.content)))
+        await before.guild.get_channel(695630766477934632).send(f"Message by `{before.author}` edited in `{before.channel.name}` at `{now.strftime('%I:%M:%S %p')}`")
+        await before.guild.get_channel(695630766477934632).send(f"**Before**\n> {discord.utils.escape_markdown(before.content)}")
+        await before.guild.get_channel(695630766477934632).send(f"**After**\n> {discord.utils.escape_markdown(after.content)}")
+
 
 @bot.event
 async def on_member_join(member: discord.Member):
     now = datetime.now()
-    await member.guild.get_channel(695630766477934632).send("User `{member}` with ID `{member.id}` joined at `{time}` :D".format(member=member,
-                                                                                 time=now.strftime("%I:%M:%S %p")))
+    await member.guild.get_channel(695630766477934632).send(f"User `{member}` with ID `{member.id}` joined at `{now.strftime('%I:%M:%S %p')}` :D")
+
 
 @bot.event
 async def on_member_remove(member: discord.Member):
     now = datetime.now()
-    await member.guild.get_channel(695630766477934632).send("User `{member}` with ID `{member.id}` left at `{time}` :(".format(member=member,
-                                                                                 time=now.strftime("%I:%M:%S %p")))
+    await member.guild.get_channel(695630766477934632).send(f"User `{member}` with ID `{member.id}` left at `{now.strftime('%I:%M:%S %p')}` :(")
 
-bot.run(bot_token)
+
+bot.run(get_token())
